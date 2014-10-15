13efd64162eb253c5ed541ffb0b7bd41cd793d37
linaelsadek <linaelsadek@cmail.carleton.com>
2014-10-15 13:15:13 -0400

    Initial Game, prints an empty board

---
+++ src/Game.java
@@ -0,0 +1,13 @@
+
+public class Game {
+	
+	
+
+	 /** The entry main method (the program starts here) */
+	   public static void main(String[] args) {
+		   Board boardGame = new Board();
+	      
+	         boardGame.printBoard();
+	        
+	   }
+}
