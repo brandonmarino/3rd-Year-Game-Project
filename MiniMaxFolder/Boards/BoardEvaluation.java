891a31d9b50e50698ec676f1ec5e9ca5e670731e
linaelsadek <linaelsadek@cmail.carleton.ca>
2014-11-06 15:17:24 -0500

    Update BoardEvaluation.java

--- MiniMaxFolder/Boards/BoardEvaluation.java
+++ MiniMaxFolder/Boards/BoardEvaluation.java
@@ -1,40 +1,58 @@
 package Boards;
 
 import Boards.Board.PLAYER;
+import Games.GameNotAbs;
 import Games.Othello;
-public class BoardEvaluation 
+public class BoardEvaluation
 {
-
-	
+	private int BoardDimension;
+	private Board cloneBoard;
 	public int evaluate (Othello game) {
 		int score = 0;
-		int BoardDimension = game.getBoardDim();
-		if (game.getBoard().hasBeenWon() == PLAYER.PLAYER1 || game.getBoard().hasBeenWon() == PLAYER.PLAYER2) {
+		BoardDimension = game.getBoardDim();
+		cloneBoard = game.getBoardGame().clone();
+		if (cloneBoard.hasBeenWon() == PLAYER.PLAYER1 || cloneBoard.hasBeenWon() == PLAYER.PLAYER2) {
 			// disks difference
 			score = game.getBoard().getBlackDiscs() - game.getBoard().getWhiteDiscs();
 		}
 					
 		else {
-			// mobility
+			// Find the total numbers of moves takes so far by each player
 			int darkMobility = game.getOAIM().getLegalMoves(game.getBoard().getBlackDiscs(), game).size();
 			int lightMobility = game.getOAIM().getLegalMoves(game.getBoard().getWhiteDiscs(), game).size();
+			//Find the difference between them
 			int mobility = darkMobility - lightMobility;
 			// stability
 			int darkStability = 0;
 			int lightStability = 0;
 			for (int i = 0; i < BoardDimension; i++) {
 				for (int j = 0; j < BoardDimension; j++) {
-					if (game.getBoard().getCell(i, j) != PLAYER.EMPTY) {
-						if ( (stableDirection(game, i, j, 1, 0) || stableDirection(game, i, j, -1, 0) ) &&
-							 (stableDirection(game, i, j, 0, 1) || stableDirection(game, i, j, 0, -1) ) &&
-							 (stableDirection(game, i, j, 1, 1) || stableDirection(game, i, j, -1, -1) ) &&
-							 (stableDirection(game, i, j, 1, -1) || stableDirection(game, i, j, -1, 1) ) ) {
+					if (cloneBoard.getCell(i, j) != PLAYER.EMPTY) {
+						if ( (stableDirection(cloneBoard, i, j, 1, 0) || stableDirection(cloneBoard, i, j, -1, 0) ) &&
+							 (stableDirection(cloneBoard, i, j, 0, 1) || stableDirection(cloneBoard, i, j, 0, -1) ) &&
+							 (stableDirection(cloneBoard, i, j, 1, 1) || stableDirection(cloneBoard, i, j, -1, -1) ) &&
+							 (stableDirection(cloneBoard, i, j, 1, -1) || stableDirection(cloneBoard, i, j, -1, 1) ) ) {
 						
-							if (game.getBoard().getCell(i, j) == PLAYER.PLAYER1) {
+							//Find how many possible moves left for player 1
+							if (cloneBoard.getCell(i, j) == PLAYER.PLAYER1) {
 								darkStability++;
-							}
+								cloneBoard.setCell(PLAYER.PLAYER1, i, j);
+								//game.getBoard().attemptMove(i, j);
+								//game.getBoard().printBoard();
+								//game.setBoardGame(cloneBoard);
+								//game.getBoard().setCell(PLAYER.PLAYER1, i, j);
+								
+								
+							}//Find how many possible moves left for player 2
 							else {
 								lightStability++;
+								//cloneBoard.attemptMove(i,j);
+								//game.getBoard().setCell(PLAYER.PLAYER2, i, j);
+								//game.setBoardGame(cloneBoard);
+								cloneBoard.setCell(PLAYER.PLAYER2, i, j);
+								//game.setBoardGame(cloneBoard);
+								//game.getBoard().attemptMove(i, j);
+								//game.getBoard().printBoard();
 							}
 						}
 					}
@@ -46,30 +64,33 @@ public class BoardEvaluation
 			// total score
 			score = (100 * mobility) + (10 * stability) + (1 * disksDiff);
 		}
+		game.setBoardGame(cloneBoard);
 		return score;
 	}
 	
-	private boolean stableDirection(Othello game, int i, int j, int v, int h) {
+	
+	//Legal moves checking
+	private boolean stableDirection(Board game, int i, int j, int v, int h) {
 		
-		int BoardDimensions =  game.getBoardDim();
+		
 		OthelloBoard player = new  OthelloBoard();
-		player.setcurrentPlayer(game.getBoard().getCell(i, j));
+		player.setcurrentPlayer(game.getCell(i, j));
 		boolean stable = true;
 		
-		while (i+v < BoardDimensions && j+h < BoardDimensions &&
-			   i+v >= 0 && j+h >= 0 &&
+				//if i+v is within range and j+h is within range
+		while (i+v < BoardDimension && j+h < BoardDimension &&
+			   i+v >= 0 && j+h >= 0 &&	
 			   stable) {
-			
-			if (game.getBoardGame().getCell(i+v, j+h) != player.getcurrentPlayer()) {
+				
+			//and the new position does not have the current player
+			if (game.getCell(i+v, j+h) != player.getcurrentPlayer()) {
 				stable = false;
 			}
-			i = i + v;
-			j = j + h;
-			game.getBoard().attemptMove(i,j);
-			
-				
+			i += v;	//find another location
+			j += h;				
 		}
+		
 		return stable;
 	
-}
+	}
 }
