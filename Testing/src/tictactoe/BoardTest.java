e1b75dd4b16b94820066b3bd0d89cf9465f0458c
linaelsadek <linaelsadek@cmail.carleton.ca>
2014-11-19 15:03:10 -0500

    invalid cases

--- Testing/src/tictactoe/BoardTest.java
+++ Testing/src/tictactoe/BoardTest.java
@@ -1,242 +1,98 @@
-package tictactoe;
+package Testing;
+import Boards.Board;
+import Boards.Board.GAME_STATE;
+import Boards.Board.PLAYER;
+import Boards.TicTacToeBoard;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class BoardTest {
+public class TicTacToeBoardTest {
 
 	@Test
 	public void ConstructorTest() {
-		Board b = new Board();
-		assertEquals(0,b.getCurrentState());
-		assertEquals(1,b.getcurrentPlayer());
+		TicTacToeBoard b = new TicTacToeBoard();
+		assertEquals(Board.GAME_STATE.PLAYING,b.getCurrentState());
+		assertEquals(Board.PLAYER.PLAYER1,b.getCurrentPlayer());
 	}
 	
-	@Test
-	public void GetterTesting()
-	{
-		Board b = new Board();
-		assertEquals(1,b.getCROSS());
-		assertEquals(2,b.getCROSSWon());
-		assertEquals(0,b.getcurrentColumn());
-		assertEquals(1,b.getcurrentPlayer());
-		assertEquals(0,b.getcurrentRow());
-		assertEquals(0,b.getCurrentState());
-		assertEquals(1,b.getDRAW());
-		assertEquals(0,b.getEMPTY());
-		assertEquals(0,b.getMoveOnBoard(1, 2));
-		assertEquals(2,b.getNOUGHT());
-		assertEquals(3,b.getNOUGHTWon());
-		assertEquals(0,b.getPLAYING());
-	}
 	
 	@Test
 	public void BasicSetterTesting()
 	{
-		Board b = new Board();
-		b.setcurrentColumn(2);
-		assertEquals(2,b.getcurrentColumn());
-		
-		b.setcurrentPlayer(2);
-		assertEquals(2,b.getcurrentPlayer());
-		
-		b.setcurrentRow(1);
-		assertEquals(1,b.getcurrentRow());
-		
-		b.setCurrentState(3);
-		assertEquals(3,b.getCurrentState());
+		TicTacToeBoard b = new TicTacToeBoard();
+		b.setcurrentPlayer(PLAYER.PLAYER2);
+		assertEquals(PLAYER.PLAYER2,b.getCurrentPlayer());
+		b.setCurrentState(GAME_STATE.DRAW);
+		assertEquals(GAME_STATE.DRAW,b.getCurrentState());
+		assertNotEquals(GAME_STATE.PLAYER1_WON,b.getCurrentState());
 	}
 	
 	
 	@Test
-	public void BoardTestAndDraw()
+	public void TicTacToeBoardTestAndDraw()
 	{
-		Board b = new Board();
+		TicTacToeBoard b = new TicTacToeBoard();
 		
-		b.setMoveOnBoard(0, 0, 1);
-		b.setMoveOnBoard(0, 1, 2);
-		b.setMoveOnBoard(0, 2, 1);
+		b.attemptMove(0, 0);
+		b.attemptMove(0, 1);
+		b.attemptMove(0, 2);
 		
-		b.setMoveOnBoard(1, 0, 2);
-		b.setMoveOnBoard(1, 1, 1);
-		b.setMoveOnBoard(1, 2, 2);
+		assertTrue(b.isHorizontal());
+		assertFalse(b.isVertical());
+		assertFalse(b.isDiagonal());
 		
-		b.setMoveOnBoard(2, 0, 1);
-		b.setMoveOnBoard(2, 1, 2);
-		b.setMoveOnBoard(2, 2, 1);
+		b.attemptMove(0, 0);
+		b.attemptMove(1, 0);
+		b.attemptMove(2, 0);
 		
-		assertTrue(b.isDraw());
+		assertTrue(b.isVertical());
+		assertFalse(b.isHorizontal());
+		assertFalse(b.isDiagonal());
 		
-		b.setMoveOnBoard(0, 0, 2);
-		b.setMoveOnBoard(0, 1, 1);
-		b.setMoveOnBoard(0, 2, 2);
+		b.attemptMove(0, 0);
+		b.attemptMove(1, 1);
+		b.attemptMove(2, 2);
 		
-		b.setMoveOnBoard(1, 0, 1);
-		b.setMoveOnBoard(1, 1, 2);
-		b.setMoveOnBoard(1, 2, 1);
-		
-		b.setMoveOnBoard(2, 0, 2);
-		b.setMoveOnBoard(2, 1, 1);
-		b.setMoveOnBoard(2, 2, 2);
-		
-		assertTrue(b.isDraw());
-		
-		b.setMoveOnBoard(2, 2, 0);
-		assertFalse(b.isDraw());
-		
-		b.setMoveOnBoard(2, 2, 2);
-		b.setMoveOnBoard(1, 1, 0);
-		assertFalse(b.isDraw());
-		
-		b.setMoveOnBoard(0, 0, 1);
-		b.setMoveOnBoard(0, 1, 2);
-		b.setMoveOnBoard(0, 2, 2);
-		
-		b.setMoveOnBoard(1, 0, 2);
-		b.setMoveOnBoard(1, 1, 1);
-		b.setMoveOnBoard(1, 2, 1);
-		
-		b.setMoveOnBoard(2, 0, 1);
-		b.setMoveOnBoard(2, 1, 1);
-		b.setMoveOnBoard(2, 2, 2);
-		
-		assertTrue(b.isDraw());
-	
+		assertTrue(b.isDiagonal());
+		assertFalse(b.isHorizontal());
+		assertFalse(b.isVertical());
 	}
 
 	@Test
-	public void BoardTestAndHasWon()
+	public void TicTacToeBoardTestAndHasWon()
 	{
-		Board b = new Board();
-		
+		TicTacToeBoard b = new TicTacToeBoard();
+		b.setcurrentPlayer(PLAYER.PLAYER1);
 		//Test 1 - horizontal
-		b.setMoveOnBoard(0, 0, 1);
-		b.setMoveOnBoard(0, 1, 1);
-		b.setMoveOnBoard(0, 2, 1);
+		b.attemptMove(0, 0);
+		b.attemptMove(0, 1);
+		b.attemptMove(0, 2);
 		
-		b.setMoveOnBoard(1, 0, 2);
-		b.setMoveOnBoard(1, 1, 1);
-		b.setMoveOnBoard(1, 2, 2);
+		b.updateGame();
+		assertTrue(b.getCurrentState() != GAME_STATE.PLAYING);
+		assertTrue(b.getCurrentState() == GAME_STATE.PLAYER1_WON);
 		
-		b.setMoveOnBoard(2, 0, 0);
-		b.setMoveOnBoard(2, 1, 0);
-		b.setMoveOnBoard(2, 2, 0);
-		
-		assertTrue(b.isHorizontal(1, 0, 0));
-		assertTrue(b.hasWon(1,0,0));
-		
-		//Test 2 - horizontal
-		b.setMoveOnBoard(0, 0, 1);
-		b.setMoveOnBoard(0, 1, 0);
-		b.setMoveOnBoard(0, 2, 1);
-		
-		b.setMoveOnBoard(1, 0, 2);
-		b.setMoveOnBoard(1, 1, 0);
-		b.setMoveOnBoard(1, 2, 2);
-		
-		b.setMoveOnBoard(2, 0, 1);
-		b.setMoveOnBoard(2, 1, 1);
-		b.setMoveOnBoard(2, 2, 1);
-		
-		assertTrue(b.isHorizontal(1, 2, 0));
-		assertTrue(b.hasWon(1,2,0));
-		
-		//Test 3 - horizontal
-		b.setMoveOnBoard(0, 0, 1);
-		b.setMoveOnBoard(0, 1, 0);
-		b.setMoveOnBoard(0, 2, 1);
-		
-		b.setMoveOnBoard(1, 0, 2);
-		b.setMoveOnBoard(1, 1, 2);
-		b.setMoveOnBoard(1, 2, 2);
-		
-		b.setMoveOnBoard(2, 0, 0);
-		b.setMoveOnBoard(2, 1, 0);
-		b.setMoveOnBoard(2, 2, 1);
-		
-		assertTrue(b.isHorizontal(2, 1, 0));
-		assertTrue(b.hasWon(2,1,0));		
-		
-		//Test 4 - vertical
-		b.setMoveOnBoard(0, 0, 0);
-		b.setMoveOnBoard(0, 1, 1);
-		b.setMoveOnBoard(0, 2, 2);
-		
-		b.setMoveOnBoard(1, 0, 2);
-		b.setMoveOnBoard(1, 1, 0);
-		b.setMoveOnBoard(1, 2, 2);
-		
-		b.setMoveOnBoard(2, 0, 2);
-		b.setMoveOnBoard(2, 1, 1);
-		b.setMoveOnBoard(2, 2, 2);
-		
-		assertTrue(b.isVertical(2, 0, 2));
-		assertTrue(b.hasWon(2,0,2));
-		
+		b = new TicTacToeBoard();
+		b.setcurrentPlayer(PLAYER.PLAYER1);
 		//Test 5 - vertical
-		b.setMoveOnBoard(0, 0, 1);
-		b.setMoveOnBoard(0, 1, 0);
-		b.setMoveOnBoard(0, 2, 0);
+		b.attemptMove(0, 0 );
+		b.attemptMove(1, 0 );
+		b.attemptMove(2, 0);
 		
-		b.setMoveOnBoard(1, 0, 1);
-		b.setMoveOnBoard(1, 1, 0);
-		b.setMoveOnBoard(1, 2, 2);
+		b.updateGame();
+		assertTrue(b.isVertical());
 		
-		b.setMoveOnBoard(2, 0, 1);
-		b.setMoveOnBoard(2, 1, 1);
-		b.setMoveOnBoard(2, 2, 2);
-		
-		assertTrue(b.isVertical(1, 0, 0));
-		assertTrue(b.hasWon(1, 0, 0));
-		
-		//Test 6 - vertical
-		b.setMoveOnBoard(0, 0, 2);
-		b.setMoveOnBoard(0, 1, 1);
-		b.setMoveOnBoard(0, 2, 0);
-		
-		b.setMoveOnBoard(1, 0, 1);
-		b.setMoveOnBoard(1, 1, 1);
-		b.setMoveOnBoard(1, 2, 2);
-		
-		b.setMoveOnBoard(2, 0, 1);
-		b.setMoveOnBoard(2, 1, 1);
-		b.setMoveOnBoard(2, 2, 2);
-		
-		assertTrue(b.isVertical(1, 0, 1));
-		assertTrue(b.hasWon(1, 0, 1));
-		
-		//Test 7 - Opposite Diagonal
-		b.setMoveOnBoard(0, 0, 0);
-		b.setMoveOnBoard(0, 1, 1);
-		b.setMoveOnBoard(0, 2, 2);
-		
-		b.setMoveOnBoard(1, 0, 2);
-		b.setMoveOnBoard(1, 1, 2);
-		b.setMoveOnBoard(1, 2, 2);
-		
-		b.setMoveOnBoard(2, 0, 2);
-		b.setMoveOnBoard(2, 1, 1);
-		b.setMoveOnBoard(2, 2, 0);
-		
-		assertTrue(b.isDiagonal(2, 0, 2));
-		assertTrue(b.hasWon(2,0,2));
-		
+		b.updateGame();
+		assertTrue(b.getCurrentState() != GAME_STATE.PLAYING);
 		//Test 8 - Diagonal
-		b.setMoveOnBoard(0, 0, 2);
-		b.setMoveOnBoard(0, 1, 1);
-		b.setMoveOnBoard(0, 2, 0);
-				
-		b.setMoveOnBoard(1, 0, 0);
-		b.setMoveOnBoard(1, 1, 2);
-		b.setMoveOnBoard(1, 2, 0);
-				
-		b.setMoveOnBoard(2, 0, 0);
-		b.setMoveOnBoard(2, 1, 1);
-		b.setMoveOnBoard(2, 2, 2);
+		b.attemptMove(0, 0);
+		b.attemptMove(1, 1);
+		b.attemptMove(2, 2);
 		
-		assertTrue(b.isDiagonal(2, 0, 0));
-		assertTrue(b.hasWon(2,0,0));
+		b.updateGame();
+		assertTrue(b.getCurrentState() != GAME_STATE.PLAYING);
 	
 	}
 
