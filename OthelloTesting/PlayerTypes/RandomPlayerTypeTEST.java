2fc6aa85e26f1d4cd9d6068f00704ecad2801483
linaelsadek <linaelsadek@cmail.carleton.ca>
2014-11-21 13:21:22 -0500

    Update RandomPlayerTypeTEST.java

--- OthelloTesting/PlayerTypes/RandomPlayerTypeTEST.java
+++ OthelloTesting/PlayerTypes/RandomPlayerTypeTEST.java
@@ -11,6 +11,8 @@ import PlayerTypes.RandomPlayerType;
 import org.junit.Before;
 import org.junit.Test;
 
+import common.Move;
+
 /**
  * *********************************************************************************************************************************************************
  * 									RandomPlayerTypeTEST 
@@ -19,6 +21,8 @@ import org.junit.Test;
  * Author: Osama Buhamad 
  * -test for RandomPlayerTypeTEST Class
  * 
+ * Edited by: Lina El Sadek
+ * for Milestone 3 
  */
 
 public class RandomPlayerTypeTest {
@@ -32,32 +36,45 @@ public class RandomPlayerTypeTest {
 	 	@Test
 	    public void testGetMove() {
 	        // test with case not enough available moves
-	        ArrayList<Integer[]> availableMoves = new ArrayList<Integer[]>();
+	        ArrayList<Move> availableMoves = new ArrayList<Move>();
 	        randomPlayer.setAvailableMoves(availableMoves);
 	        assertNull(randomPlayer.getMove());
 	        
 	        // test with case have an available moves
-	        availableMoves.add(new Integer[] { 1 });
+	        availableMoves.add(new Move( 1,2 ));
 	        randomPlayer.setAvailableMoves(availableMoves);
-	        int move = randomPlayer.getMove()[0];
-	        assertEquals(1, move);
+	        Move move = randomPlayer.getMove();
+	        Move temp = new Move(1,2);
+	        assertEquals(temp, move);
 	        //assertTrue(randomPlayer.getAvailableMoves().isEmpty());
 	        
 	        // test another case
-	        availableMoves.add(new Integer[] { 1 });
-	        availableMoves.add(new Integer[] { 2 });
+	        availableMoves.add(new Move (1,2));
+	        availableMoves.add(new Move(2,1));
 	        randomPlayer.setAvailableMoves(availableMoves);
 	        randomPlayer.getMove();
 	       // assertEquals(1, randomPlayer.getAvailableMoves().size());
 	        randomPlayer.getMove();
 	       // assertTrue(randomPlayer.getAvailableMoves().isEmpty());
 	        
-	        availableMoves.add(new Integer[] { 2,3 });
+	        availableMoves.add(new Move(2,3));
 	        randomPlayer.setAvailableMoves(availableMoves);
-	        int move1 = randomPlayer.getMove()[1];
-	        assertNotEquals(2, move1);	//testing for an invalid value
-	        assertEquals(3, move1);
+	       Move move1 = randomPlayer.getMove();
+	        assertNotEquals(temp, move1);	//testing for an invalid value
+	        temp = null;
+	        assertNull(temp);
+	        assertNotEquals(temp, move1);
+	        temp = move1;
+	        assertEquals(temp, move1);
+	        assertNotNull(temp);
 	        
+	       Move anotherMove = new Move();
+	       temp.setRow(0);
+	       temp.setColumn(0);
+	       assertNotEquals(temp, anotherMove);
+	       temp.setRow(-1);
+	       temp.setColumn(-1);
+	       assertEquals(temp, anotherMove);
 	 }
 
 }
