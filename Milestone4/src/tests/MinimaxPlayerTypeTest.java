816f27c8a1e3027b26c75938af7f0993c4d15c20
linaelsadek <linaelsadek@cmail.carleton.ca>
2014-12-07 10:36:14 -0500

    Create MinimaxPlayerTypeTest.java

---
+++ Milestone4/src/tests/MinimaxPlayerTypeTest.java
@@ -0,0 +1,52 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
+import org.junit.*;
+
+import Boards.Board;
+import PlayerTypes.HumanPlayerType;
+import PlayerTypes.PlayerType;
+import Strategies.Minimax.MinimaxPlayerType;
+
+public class MinimaxPlayerTypeTest {
+	
+	private MinimaxPlayerType MPT;
+	private PlayerType PT;
+	private Board board;
+	
+	@Before
+	public void setUp()
+	{
+		try
+		{
+		String data = "John";
+		InputStream testInput;
+		testInput = new ByteArrayInputStream(data.getBytes("UTF-8"));
+		System.setIn(testInput);
+		PT = new HumanPlayerType(board, 1);
+		MPT = new MinimaxPlayerType(PT.getBoard(), 1);
+		}
+		catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void constructorTest()
+	{
+		assertNotNull(MPT);
+		assertNotNull(PT);	
+	}
+	
+	@Test
+	public void  getMoveTest()
+	{
+		assertNull(MPT.getMove());
+	}
+
+}
