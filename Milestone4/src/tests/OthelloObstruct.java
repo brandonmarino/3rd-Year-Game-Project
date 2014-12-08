26fa08a072401d144a80e6838aed44c4266270c2
linaelsadek <linaelsadek@cmail.carleton.ca>
2014-12-08 18:58:35 -0500

    Create OthelloObstruct.java

---
+++ Milestone4/src/tests/OthelloObstruct.java
@@ -0,0 +1,40 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import Strategies.Alternative.ObstructPlayerType;
+import Strategies.Alternative.OthelloObstruct;
+import Boards.Board;
+
+/**
+ * OthelloObstructTest.java
+ * @author Lina El Sadek
+ *
+ */
+public class OthelloObstructTest {
+	private Board board;
+	private ObstructPlayerType OPT;
+	private OthelloObstruct OO;
+
+	@Before
+	public void setUp()
+	{
+		assertNull(board);
+		assertNull(OPT);
+		assertNull(OO);
+		OPT = new ObstructPlayerType(board, 0);
+		OO = new OthelloObstruct(OPT);
+	}
+	
+	@Test
+	public void constructorTest()
+	{
+		
+		assertNull(board);
+		assertNotNull(OPT);
+		assertNotNull(OO);
+		
+	}
+}
