541d4b0e425166098d828851c7675e88cd10ad16
linaelsadek <linaelsadek@cmail.carleton.ca>
2014-12-08 19:48:12 -0500

    Update RandomPlayerTypeTest.java

--- Milestone4/src/tests/RandomPlayerTypeTest.java
+++ Milestone4/src/tests/RandomPlayerTypeTest.java
@@ -3,9 +3,13 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 import Boards.Board;
+import Games.Othello;
 import PlayerTypes.PlayerType;
 import PlayerTypes.RandomPlayerType;
 
@@ -28,23 +32,39 @@ import common.Move;
 
 public class RandomPlayerTypeTest {
 	private PlayerType randomPlayer; 
+	private Othello Oth;
 	private Board board;
 	 	@Before
-	    public void setUp() {
+	    public void setUp() throws UnsupportedEncodingException {
+			
+			
+			InputStream old = System.in;
+			try {
+				String data = "2\r\n2";
+				InputStream testInput = new ByteArrayInputStream(data.getBytes("UTF-8"));
+			
+				System.setIn(testInput);
+				
 		 randomPlayer = new RandomPlayerType(board,0);
+		 Oth = new Othello();
+		 } finally {
+				System.setIn(old);
+			}
+			
 	    }
 
 	 	@Test
 	    public void testGetMove() {
+	 		
 	        // test with case not enough available moves
 	        ArrayList<Move> availableMoves = new ArrayList<Move>();
 	        randomPlayer.setAvailableMoves(availableMoves);
-	        assertNull(randomPlayer.getMove(null));
+	        assertNull(randomPlayer.getMove(Oth));
 	        
 	        // test with case have an available moves
 	        availableMoves.add(new Move( 1,2 ));
 	        randomPlayer.setAvailableMoves(availableMoves);
-	        Move move = randomPlayer.getMove(null);
+	        Move move = randomPlayer.getMove(Oth);
 	        Move temp = new Move(1,2);
 	        assertEquals(temp, move);
 	        //assertTrue(randomPlayer.getAvailableMoves().isEmpty());
@@ -53,14 +73,14 @@ public class RandomPlayerTypeTest {
 	        availableMoves.add(new Move (1,2));
 	        availableMoves.add(new Move(2,1));
 	        randomPlayer.setAvailableMoves(availableMoves);
-	        randomPlayer.getMove(null);
+	        randomPlayer.getMove(Oth);
 	       // assertEquals(1, randomPlayer.getAvailableMoves().size());
-	        randomPlayer.getMove(null);
+	        randomPlayer.getMove(Oth);
 	       // assertTrue(randomPlayer.getAvailableMoves().isEmpty());
 	        
 	        availableMoves.add(new Move(2,3));
 	        randomPlayer.setAvailableMoves(availableMoves);
-	       Move move1 = randomPlayer.getMove(null);
+	       Move move1 = randomPlayer.getMove(Oth);
 	        assertNotEquals(temp, move1);	//testing for an invalid value
 	        temp = null;
 	        assertNull(temp);
