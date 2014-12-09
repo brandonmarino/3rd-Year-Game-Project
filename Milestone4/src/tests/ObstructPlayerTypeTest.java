9dc159c53450f4e5f414524a6acea00830ac2aa3
linaelsadek <linaelsadek@cmail.carleton.ca>
2014-12-08 19:51:55 -0500

    Update ObstructPlayerTypeTest.java

--- Milestone4/src/tests/ObstructPlayerTypeTest.java
+++ Milestone4/src/tests/ObstructPlayerTypeTest.java
@@ -1,7 +1,10 @@
-package Testing;
+package tests;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 import org.junit.*;
@@ -9,6 +12,8 @@ import org.junit.*;
 import common.Move;
 import Strategies.Alternative.ObstructPlayerType;
 import Boards.Board;
+import Games.Othello;
+import Games.TicTacToe;
 
 /**
  * ObstructPlayerType.java
@@ -18,25 +23,40 @@ import Boards.Board;
 public class ObstructPlayerTypeTest {
 	private Board board;
 	private ObstructPlayerType OPT;
-	
+	private Othello oth;
 	@Before
-	public void setUp()
-	{
+	public void setUp()throws UnsupportedEncodingException {
+		
+		
+		InputStream old = System.in;
+		try {
+			String data = "2\r\n2";
+			InputStream testInput = new ByteArrayInputStream(data.getBytes("UTF-8"));
+		
+			System.setIn(testInput);
+			
+	
 		OPT = new ObstructPlayerType(board, 0);
+		oth = new Othello();
+		} finally {
+			System.setIn(old);
+		}
 	}
 	
 	@Test
 	public void constructorTest()
 	{
 		assertNull(board);
-		assertNotNull(OPT);	
+		assertNotNull(OPT);
+		//assertNull(oth);
 	}
 	
 	@Test
 	public void getMoveTest()
 	{
 		Move m = new Move(0,0);
-		m = OPT.getMove();
+		
+		m = OPT.getMove(oth);
 		assertNull(m);
 		ArrayList<Move> move = new ArrayList<Move>();
 		move = OPT.getAvailableMoves();
